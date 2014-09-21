6a77868d322dc308b4f316791c17dbbeef9cf527
corti <corti@5d055bd7-163f-4d8d-bd86-7d408c05c84d>
2014-09-21 12:16:10 +0000

    Added a YUM mock

---
+++ t/mocks/yum_errors.1.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+cat 1>&2 <<EOF
+Unable to read consumer identity
+Unable to read consumer identity
+EOF
+
+cat <<EOF
+ftp.x86_64
+mlocate.x86_64
+python-mako.noarch
+python-zope-interface.x86_64
+wget.x86_64
+Obsoleting
+python-zope-interface.x86_64
+EOF
+
