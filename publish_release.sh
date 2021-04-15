06f1c59633dc9f99ace54296b022bbd485188d68
matteo@corti.li <matteo@corti.li>
2021-04-15 11:29:16 +0200

    Added the script to publish a new release

---
+++ publish_release.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+VERSION=$( grep "our\ \$VERSION\ =\ " check_updates | sed "s/^[^']*'\([0-9.]*\)';/\1/" )
+
+echo 'Did you update the RELEASE_NOTES.md? '
+read -r ANSWER
+if [ "x${ANSWER}" = "xy" ] ; then
+
+    perl Makefile.PL
+    make dist
+    gh release create "v${VERSION}" --title "check_updates-${VERSION}" --notes-file RELEASE_NOTES.md "check_updates-${VERSION}.tar.gz"
+
+fi
